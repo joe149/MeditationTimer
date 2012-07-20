f6afc74ba2bb2e88d0cb661f3ae9a85f5f9d61ed
joe149 <joe.elwell@gmail.com>
2012-07-19 18:36:54 -0700

    Initial Commit

---
+++ MeditationTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MeditationTimer
+//
+//  Created by Joe Elwell on Jul/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
